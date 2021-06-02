e3c2da5b2e6931299010a5cd2266b4dae8a7edf6
szymonkacperek <szymon.kacperek96@gmail.com>
2021-06-02 10:54:31 +0200

    Add database connection and display data in UI

---
+++ README.txt
@@ -0,0 +1,5 @@
+Database management with user interface using Visual Studio Community 2019 and MSSQL Server 2018
+
+==============
+
+Load TSQL.bak into MSSQL with `Restore Database` option.
---
+++ sqlserver-vs-ui/MyForm.h
@@ -0,0 +1,285 @@
+#pragma once
+
+namespace sqlservervsui {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+	using namespace System::Data::SqlClient;
+	using namespace System::Data::Odbc;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::BindingSource^ bindingSource1;
+	protected:
+	private: System::Windows::Forms::BindingNavigator^ bindingNavigator1;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorAddNewItem;
+	private: System::Windows::Forms::ToolStripLabel^ bindingNavigatorCountItem;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorDeleteItem;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorMoveFirstItem;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorMovePreviousItem;
+	private: System::Windows::Forms::ToolStripSeparator^ bindingNavigatorSeparator;
+	private: System::Windows::Forms::ToolStripTextBox^ bindingNavigatorPositionItem;
+	private: System::Windows::Forms::ToolStripSeparator^ bindingNavigatorSeparator1;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorMoveNextItem;
+	private: System::Windows::Forms::ToolStripButton^ bindingNavigatorMoveLastItem;
+	private: System::Windows::Forms::ToolStripSeparator^ bindingNavigatorSeparator2;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Data::DataSet^ dataSet1;
+	private: System::Windows::Forms::ListBox^ listBox1;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::ComponentModel::IContainer^ components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
+			this->bindingSource1 = (gcnew System::Windows::Forms::BindingSource(this->components));
+			this->dataSet1 = (gcnew System::Data::DataSet());
+			this->bindingNavigator1 = (gcnew System::Windows::Forms::BindingNavigator(this->components));
+			this->bindingNavigatorAddNewItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorCountItem = (gcnew System::Windows::Forms::ToolStripLabel());
+			this->bindingNavigatorDeleteItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMoveFirstItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMovePreviousItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorSeparator = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->bindingNavigatorPositionItem = (gcnew System::Windows::Forms::ToolStripTextBox());
+			this->bindingNavigatorSeparator1 = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->bindingNavigatorMoveNextItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorMoveLastItem = (gcnew System::Windows::Forms::ToolStripButton());
+			this->bindingNavigatorSeparator2 = (gcnew System::Windows::Forms::ToolStripSeparator());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->listBox1 = (gcnew System::Windows::Forms::ListBox());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataSet1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingNavigator1))->BeginInit();
+			this->bindingNavigator1->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// bindingSource1
+			// 
+			this->bindingSource1->DataSource = this->dataSet1;
+			this->bindingSource1->Position = 0;
+			// 
+			// dataSet1
+			// 
+			this->dataSet1->DataSetName = L"NewDataSet";
+			// 
+			// bindingNavigator1
+			// 
+			this->bindingNavigator1->AddNewItem = this->bindingNavigatorAddNewItem;
+			this->bindingNavigator1->BindingSource = this->bindingSource1;
+			this->bindingNavigator1->CountItem = this->bindingNavigatorCountItem;
+			this->bindingNavigator1->DeleteItem = this->bindingNavigatorDeleteItem;
+			this->bindingNavigator1->Items->AddRange(gcnew cli::array< System::Windows::Forms::ToolStripItem^  >(11) {
+				this->bindingNavigatorMoveFirstItem,
+					this->bindingNavigatorMovePreviousItem, this->bindingNavigatorSeparator, this->bindingNavigatorPositionItem, this->bindingNavigatorCountItem,
+					this->bindingNavigatorSeparator1, this->bindingNavigatorMoveNextItem, this->bindingNavigatorMoveLastItem, this->bindingNavigatorSeparator2,
+					this->bindingNavigatorAddNewItem, this->bindingNavigatorDeleteItem
+			});
+			this->bindingNavigator1->Location = System::Drawing::Point(0, 0);
+			this->bindingNavigator1->MoveFirstItem = this->bindingNavigatorMoveFirstItem;
+			this->bindingNavigator1->MoveLastItem = this->bindingNavigatorMoveLastItem;
+			this->bindingNavigator1->MoveNextItem = this->bindingNavigatorMoveNextItem;
+			this->bindingNavigator1->MovePreviousItem = this->bindingNavigatorMovePreviousItem;
+			this->bindingNavigator1->Name = L"bindingNavigator1";
+			this->bindingNavigator1->PositionItem = this->bindingNavigatorPositionItem;
+			this->bindingNavigator1->Size = System::Drawing::Size(1235, 25);
+			this->bindingNavigator1->TabIndex = 0;
+			this->bindingNavigator1->Text = L"bindingNavigator1";
+			// 
+			// bindingNavigatorAddNewItem
+			// 
+			this->bindingNavigatorAddNewItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorAddNewItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorAddNewItem.Image")));
+			this->bindingNavigatorAddNewItem->Name = L"bindingNavigatorAddNewItem";
+			this->bindingNavigatorAddNewItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorAddNewItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorAddNewItem->Text = L"Add new";
+			// 
+			// bindingNavigatorCountItem
+			// 
+			this->bindingNavigatorCountItem->Name = L"bindingNavigatorCountItem";
+			this->bindingNavigatorCountItem->Size = System::Drawing::Size(35, 22);
+			this->bindingNavigatorCountItem->Text = L"of {0}";
+			this->bindingNavigatorCountItem->ToolTipText = L"Total number of items";
+			// 
+			// bindingNavigatorDeleteItem
+			// 
+			this->bindingNavigatorDeleteItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorDeleteItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorDeleteItem.Image")));
+			this->bindingNavigatorDeleteItem->Name = L"bindingNavigatorDeleteItem";
+			this->bindingNavigatorDeleteItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorDeleteItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorDeleteItem->Text = L"Delete";
+			// 
+			// bindingNavigatorMoveFirstItem
+			// 
+			this->bindingNavigatorMoveFirstItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveFirstItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMoveFirstItem.Image")));
+			this->bindingNavigatorMoveFirstItem->Name = L"bindingNavigatorMoveFirstItem";
+			this->bindingNavigatorMoveFirstItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveFirstItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveFirstItem->Text = L"Move first";
+			// 
+			// bindingNavigatorMovePreviousItem
+			// 
+			this->bindingNavigatorMovePreviousItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMovePreviousItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMovePreviousItem.Image")));
+			this->bindingNavigatorMovePreviousItem->Name = L"bindingNavigatorMovePreviousItem";
+			this->bindingNavigatorMovePreviousItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMovePreviousItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMovePreviousItem->Text = L"Move previous";
+			// 
+			// bindingNavigatorSeparator
+			// 
+			this->bindingNavigatorSeparator->Name = L"bindingNavigatorSeparator";
+			this->bindingNavigatorSeparator->Size = System::Drawing::Size(6, 25);
+			// 
+			// bindingNavigatorPositionItem
+			// 
+			this->bindingNavigatorPositionItem->AccessibleName = L"Position";
+			this->bindingNavigatorPositionItem->AutoSize = false;
+			this->bindingNavigatorPositionItem->Font = (gcnew System::Drawing::Font(L"Segoe UI", 9));
+			this->bindingNavigatorPositionItem->Name = L"bindingNavigatorPositionItem";
+			this->bindingNavigatorPositionItem->Size = System::Drawing::Size(50, 23);
+			this->bindingNavigatorPositionItem->Text = L"1";
+			this->bindingNavigatorPositionItem->ToolTipText = L"Current position";
+			// 
+			// bindingNavigatorSeparator1
+			// 
+			this->bindingNavigatorSeparator1->Name = L"bindingNavigatorSeparator1";
+			this->bindingNavigatorSeparator1->Size = System::Drawing::Size(6, 25);
+			// 
+			// bindingNavigatorMoveNextItem
+			// 
+			this->bindingNavigatorMoveNextItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveNextItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMoveNextItem.Image")));
+			this->bindingNavigatorMoveNextItem->Name = L"bindingNavigatorMoveNextItem";
+			this->bindingNavigatorMoveNextItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveNextItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveNextItem->Text = L"Move next";
+			// 
+			// bindingNavigatorMoveLastItem
+			// 
+			this->bindingNavigatorMoveLastItem->DisplayStyle = System::Windows::Forms::ToolStripItemDisplayStyle::Image;
+			this->bindingNavigatorMoveLastItem->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"bindingNavigatorMoveLastItem.Image")));
+			this->bindingNavigatorMoveLastItem->Name = L"bindingNavigatorMoveLastItem";
+			this->bindingNavigatorMoveLastItem->RightToLeftAutoMirrorImage = true;
+			this->bindingNavigatorMoveLastItem->Size = System::Drawing::Size(23, 22);
+			this->bindingNavigatorMoveLastItem->Text = L"Move last";
+			// 
+			// bindingNavigatorSeparator2
+			// 
+			this->bindingNavigatorSeparator2->Name = L"bindingNavigatorSeparator2";
+			this->bindingNavigatorSeparator2->Size = System::Drawing::Size(6, 25);
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(12, 39);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(190, 53);
+			this->button1->TabIndex = 1;
+			this->button1->Text = L"Load database";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// listBox1
+			// 
+			this->listBox1->FormattingEnabled = true;
+			this->listBox1->Location = System::Drawing::Point(12, 98);
+			this->listBox1->Name = L"listBox1";
+			this->listBox1->Size = System::Drawing::Size(190, 303);
+			this->listBox1->TabIndex = 2;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(208, 39);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(1016, 364);
+			this->dataGridView1->TabIndex = 3;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1235, 413);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->listBox1);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->bindingNavigator1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingSource1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataSet1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->bindingNavigator1))->EndInit();
+			this->bindingNavigator1->ResumeLayout(false);
+			this->bindingNavigator1->PerformLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		try {
+		SqlConnection^ conDatabase = gcnew SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=TSQL;Integrated Security=SSPI");
+		SqlCommand^ cmd = gcnew SqlCommand("select * from HR.Employees", conDatabase);
+		SqlDataAdapter^ sda = gcnew SqlDataAdapter();
+		sda->SelectCommand = cmd;
+		sda->Fill(dataSet1);
+		bindingSource1->DataMember = dataSet1->Tables[0]->TableName;
+		dataGridView1->DataSource = bindingSource1;
+
+		listBox1->DataSource = dataSet1->Tables[0];
+		listBox1->DisplayMember = "lastname";
+		}
+		catch (Exception^ ex) {
+			MessageBox::Show(ex->Message);
+		}
+	
+		
+
+	}
+};
+}
